6a2de03263822a9941324f38a5ab835c089a554e
muhamad luthfi sadli <luthfisad1806@gmail.com>
2025-02-07 21:09:04 +0700

    fix: update only user

--- src/app/onts/ontsRoute.ts
+++ src/app/onts/ontsRoute.ts
@@ -2,13 +2,14 @@ import { Router } from "express";
 import { deleteOntController, getOntByIdController, getOntController, updateOntController, createOntController } from "./ontsController";
 import { CatchWrapper } from "../../utils/CatchWrapper";
 import { VerifyToken } from "../../middleware/verifyToken";
+import { VerifyUser } from "../../middleware/VerifyRole";
 
 const route = Router()
 
 route.get("/", VerifyToken, CatchWrapper(getOntController))
 route.get("/:id", VerifyToken, CatchWrapper(getOntByIdController))
 route.post("/", VerifyToken, CatchWrapper(createOntController))
-route.put("/:id", VerifyToken, CatchWrapper(updateOntController))
+route.put("/:id", VerifyUser, CatchWrapper(updateOntController))
 route.delete("/:id", VerifyToken, CatchWrapper(deleteOntController))
 
 export default route
--- src/app/stbs/stbsRoute.ts
+++ src/app/stbs/stbsRoute.ts
@@ -2,13 +2,14 @@ import { Router } from "express";
 import { deleteStbController, getStbByIdController, getStbController, updateStbController, createStbController } from "./stbsController";
 import { CatchWrapper } from "../../utils/CatchWrapper";
 import { VerifyToken } from "../../middleware/verifyToken";
+import { VerifyUser } from "../../middleware/VerifyRole";
 
 const route = Router()
 
 route.get("/", VerifyToken, CatchWrapper(getStbController))
 route.get("/:id", VerifyToken, CatchWrapper(getStbByIdController))
 route.post("/", VerifyToken, CatchWrapper(createStbController))
-route.put("/:id", VerifyToken, CatchWrapper(updateStbController))
+route.put("/:id", VerifyUser, CatchWrapper(updateStbController))
 route.delete("/:id", VerifyToken, CatchWrapper(deleteStbController))
 
 export default route
